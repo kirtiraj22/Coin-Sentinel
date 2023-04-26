0e094065784ccd75d1ad517c0c6abff71818fa27
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-04-26 23:38:29 +0530

    started with context API

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,41 +1,55 @@
 import React from 'react'
 import { useNavigate } from "react-router-dom"
+import { makeStyles } from 'tss-react/mui';
 import { AppBar,MenuItem, Container, Toolbar, Typography, Select, createTheme, ThemeProvider } from '@mui/material';
-import "./header.css"
-const Header = () => {
-    const navigate = useNavigate();
-    const darkTheme = createTheme({
-        palette: {
-            primary: {
-                main: "#fff",
-            },
-            type: "dark",
+
+const useStyles = makeStyles()((theme)=>{
+    return{
+        title: {
+            flex: 1,
+            color: "gold",
+            fontFamily: "Montserrat",
+            fontWeight: "bold",
+            cursor:"pointer",
         },
-    })
-  return (
-    <ThemeProvider theme={darkTheme}>
-    <AppBar color="transparent" position='static'>
-        <Container>
-            <Toolbar>
-                <Typography onClick={()=> navigate("/")} className="title">
-                    CoinSentinel
-                </Typography>
-                <Select labelId="demo-simple-select-label"
-    id="demo-simple-select"
-    label="Age" 
-                variant="outlined" 
-                style={{
-                    width:100,
-                    height:40,
-                    marginLeft:15
-                }}>
+    };
+});
+
+const darkTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#fff",
+        },
+        type: "dark",
+    },
+})
+const Header = () => {
+    const { classes } = useStyles();
+    const navigate = useNavigate();
+    return (
+        <ThemeProvider theme={darkTheme}>
+        <AppBar color="transparent" position='static'>
+            <Container>
+                <Toolbar>
+                    <Typography onClick={()=> navigate("/")} variant="h6" className={ classes.title }>
+                        CoinSentinel
+                    </Typography>
+                    <Select labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        label="Age" 
+                        variant="outlined" 
+                        style={{
+                            width:100,
+                            height:40,
+                            marginRight:15
+                        }}>
                     <MenuItem value={"USD"}>USD</MenuItem>
                     <MenuItem value={"INR"}>INR</MenuItem>
-                </Select>
-            </Toolbar>
-        </Container>
-    </AppBar>
-    </ThemeProvider>
+                    </Select>
+                </Toolbar>
+            </Container>
+        </AppBar>
+        </ThemeProvider>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import CryptoContext from './CryptoContext.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CryptoContext>
+      <App />
+    </CryptoContext>
   </React.StrictMode>,
 )
